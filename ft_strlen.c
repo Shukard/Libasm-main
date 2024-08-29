236b02721e13d0bfb864261c90a0c052d465ed4d
Ronan <82673003+Shukard@users.noreply.github.com>
2024-08-29 16:30:01 +0200

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -5,7 +5,7 @@ extern size_t ft_strlen(const char *);
 
 int main() {
     const char *str = "Hello, World!";
-    printf("Length of '%s' with the asm strlen : %lu\n", str, ft_strlen(str));
-	printf("Length of '%s' with the classic strlen : %lu\n", str, strlen(str));
+    printf("Length of '%s' with the ASM strlen : %lu\n", str, ft_strlen(str));
+    printf("Length of '%s' with the C strlen : %lu\n", str, strlen(str));
     return 0;
 }
