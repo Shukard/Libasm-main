584f167dc2695801322460b4b044c08fdb4984b3
Ronan <82673003+Shukard@users.noreply.github.com>
2024-09-02 16:09:03 +0200

    Update ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -8,15 +8,15 @@ extern char *ft_strcpy(char *, const char *);
 int main() {
     char src[MAX_SIZE] = "Hello, World!";
     char dst_asm[MAX_SIZE] = {0};
-	char dst_c[MAX_SIZE] = {0};
+    char dst_c[MAX_SIZE] = {0};
     char expected[MAX_SIZE] = "Hello, World!";
 	
 
     printf("Result of the ASM strcpy : %s\n", ft_strcpy(&dst_asm[0], &src[0]));
     printf("Result of the C strcpy : %s\n", strcpy(&dst_c[0], &src[0]));
 	
-	printf("Length of the copy of the ASM strcpy : %lu\n", strlen(dst_asm));
-	printf("Length of the copy of the C strcpy : %lu\n", strlen(dst_c));
+    printf("Length of the copy of the ASM strcpy : %lu\n", strlen(dst_asm));
+    printf("Length of the copy of the C strcpy : %lu\n", strlen(dst_c));
 
 	
     if (strcmp(dst_asm, expected) == 0) {
