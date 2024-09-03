7404842b0ceb1805df650188762d6a9daf19761c
Ronan <82673003+Shukard@users.noreply.github.com>
2024-09-03 18:01:02 +0200

    Update ft_write.c

--- ft_write.c
+++ ft_write.c
@@ -19,22 +19,24 @@ int main () {
 	printf("\n\n");
 	
 	printf("Exit Status of the ASM write for a str element : '%zd'\n", asm_ext_status);
-	printf("Exit Status of the C write for a str element : '%zd'\n", c_ext_status);
+	printf("Exit Status of the C write for a str element : '%zd'\n\n", c_ext_status);
 
 	printf("Output of the ASM write with an int element : '%d'\n", nb);
 	asm_ext_status = ft_write(1, &nb, sizeof(int));
-	printf("\n");
+	printf("\n\n");
 	printf("Output of the C write with an int element : '%d'\n", nb);
 	c_ext_status = write(1, &nb, sizeof(int));
+	printf("\n\n");
 
 	printf("Exit Status of the ASM write for an int element : '%zd'\n", asm_ext_status);
-	printf("Exit Status of the C write for an int element : '%zd'\n", c_ext_status);
+	printf("Exit Status of the C write for an int element : '%zd'\n\n", c_ext_status);
 
 	printf("Output of the ASM write with a void * element : '%p'\n", ptr);
 	asm_ext_status = ft_write(1, ptr, sizeof(void *));
-	printf("\n");
+	printf("\n\n");
 	printf("Output of the C write with a void * element : '%p'\n", ptr);
 	c_ext_status = write(1, ptr, sizeof(void *));
+	printf("\n\n");
 
 	printf("Exit Status of the ASM write for a void * element : '%zd'\n", asm_ext_status);
 	printf("Exit Status of the C write for a void * element : '%zd'\n", c_ext_status);
